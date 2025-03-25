a476cf9c7872d75d62d750897d7ff28565afc876
Flodurs <76902177+Flodurs@users.noreply.github.com>
2025-03-25 01:43:51 +0100

    edel

---
+++ REINFORCE.py
@@ -0,0 +1,129 @@
+import torch
+import numpy as np
+
+class model(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(model, self).__init__()
+
+        self.linear_relu_stack = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, output_dim),
+            torch.nn.Softmax(dim=0)
+        )
+
+    def forward(self, x):
+        x = self.linear_relu_stack(x)
+        return x
+
+class value_model(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(value_model, self).__init__()
+
+        self.linear_relu_stack = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, 30),
+            torch.nn.ReLU(),
+            torch.nn.Linear(30, output_dim)
+        )
+
+    def forward(self, x):
+        x = self.linear_relu_stack(x)
+        return x
+
+
+class REINFORCE:
+    def __init__(self, input_dim, output_dim, lr_policy, lr_value, baseline_mode=1):
+        self.policy_model = model(input_dim, output_dim)
+        self.optimizer_policy = torch.optim.Adam(self.policy_model.parameters(), lr=lr_policy)
+        self.baseline_mode = baseline_mode
+        
+        if self.baseline_mode == 2:
+            self.value_model = value_model(input_dim, 1)
+            self.optimizer_value = torch.optim.Adam(self.value_model.parameters(), lr=lr_value)
+
+        if self.baseline_mode == 1:
+            self.past_cum_rew = []
+
+    def ask(self, state):
+        input = torch.Tensor(state)
+        output = self.policy_model.forward(input)
+        return self.sample_from_dist(output.tolist())
+    
+    def sample_from_dist(self, dist):
+        action_probability_dist = [p/sum(dist) for p in dist]
+        action = np.random.choice([0, 1], 1, p=action_probability_dist)
+        return action[0]
+    
+    def train_on_trajectorys(self, trajectorys):
+        if self.baseline_mode == 0:
+            self.train_vanilla_REINFORCE(trajectorys)
+        if self.baseline_mode == 1:
+            self.train_avg_baseline_REINFORCE(trajectorys)
+        if self.baseline_mode == 2:
+            self.train_learned_baseline_REINFORCE(trajectorys)
+    
+    def train_vanilla_REINFORCE(self, trajectorys):
+        loss_policy = torch.Tensor([0.0])
+        for trajectory in trajectorys:
+            cumulative_reward = torch.tensor(sum([x.reward for x in trajectory]))
+            for p in trajectory:
+                # update policy model
+                probability_dist = self.policy_model(torch.Tensor(p.state))
+                prob_sum = torch.sum(probability_dist)
+                probability_dist = probability_dist/prob_sum 
+                loss_policy += torch.log(probability_dist[p.action])*cumulative_reward
+        loss_policy *= -1/len(trajectorys)
+        self.optimizer_policy.zero_grad()
+        loss_policy.backward()
+        self.optimizer_policy.step()
+
+    def train_avg_baseline_REINFORCE(self, trajectorys):
+        loss_policy = torch.Tensor([0.0])
+        for trajectory in trajectorys:
+            cumulative_reward = torch.tensor(sum([x.reward for x in trajectory]))
+            # for avg baseline
+            if len(self.past_cum_rew) > 100:
+                self.past_cum_rew.pop(0)
+            self.past_cum_rew.append(cumulative_reward)
+            baseline = sum(self.past_cum_rew)/len(self.past_cum_rew)
+            for p in trajectory:
+                advantage = cumulative_reward - baseline
+                # update policy model
+                probability_dist = self.policy_model(torch.Tensor(p.state))
+                prob_sum = torch.sum(probability_dist)
+                probability_dist = probability_dist/prob_sum 
+                loss_policy += torch.log(probability_dist[p.action])*advantage
+        loss_policy *= -1/len(trajectorys)
+        self.optimizer_policy.zero_grad()
+        loss_policy.backward()
+        self.optimizer_policy.step()
+        
+
+    def train_learned_baseline_REINFORCE(self, trajectorys):
+        loss_policy = torch.Tensor([0.0])
+        loss_value = torch.Tensor([0.0])
+        for trajectory in trajectorys:
+            cumulative_reward = torch.tensor(sum([x.reward for x in trajectory]))
+            for p in trajectory:
+                # update value model
+                value_estimate = self.value_model(torch.Tensor(p.state))
+                expectation_offset = cumulative_reward - value_estimate
+                loss_value += 0.5*(expectation_offset)**2
+                # update policy model
+                probability_dist = self.policy_model(torch.Tensor(p.state))
+                prob_sum = torch.sum(probability_dist)
+                probability_dist = probability_dist/prob_sum 
+                loss_policy += torch.log(probability_dist[p.action])*expectation_offset.detach()
+        loss_policy *= -1/len(trajectorys)
+        loss_value *= 1/len(trajectorys)
+        self.optimizer_policy.zero_grad()
+        self.optimizer_value.zero_grad()
+        loss_policy.backward()
+        loss_value.backward() 
+        self.optimizer_policy.step()
+        self.optimizer_value.step()
+        
---
+++ gym_test.py
@@ -0,0 +1,56 @@
+import gymnasium as gym
+import REINFORCE 
+import matplotlib.pyplot as plt
+
+class sar:
+    def __init__(self, s, a, r):
+        self.state = s
+        self.action = a
+        self.reward = r
+
+
+fig, ax = plt.subplots()
+plot_data = []
+plt.ion()
+plt.show()
+
+
+# Initialise the environment
+env = gym.make("CartPole-v1", render_mode="human")
+
+# stuff
+latest_trajectory = []
+trajectorys = []
+trajectory_sample_num = 5
+agent = REINFORCE.REINFORCE(4,2, 0.01, 0.01, baseline_mode=2)
+# Reset the environment to generate the first observation
+old_observation, info = env.reset()
+for i in range(10000000):
+    # this is where you would insert your policy
+
+    action = agent.ask(old_observation)
+    # step (transition) through the environment with the action
+    # receiving the next observation, reward and if the episode has terminated or truncated
+    observation, reward, terminated, truncated, info = env.step(action)
+    s = sar(old_observation, action, reward)
+    latest_trajectory.append(s)
+    old_observation = observation
+
+    # If the episode has ended then we can reset to start a new episode
+    if terminated or truncated:
+        old_observation, info = env.reset()
+        trajectorys.append(latest_trajectory)
+        latest_trajectory = []
+    if len(trajectorys) == trajectory_sample_num:
+        agent.train_on_trajectorys(trajectorys)
+        print("training ... ")
+        avg_reward = 0.0
+        for t in trajectorys:
+            avg_reward += sum([p.reward for p in t])
+        plot_data.append(avg_reward/trajectory_sample_num)
+        ax.clear()
+        ax.plot(plot_data)
+        fig.canvas.draw()
+        trajectorys = []
+
+env.close()
